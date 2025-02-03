ff2d28bbd718d71107971a9ac10483839aec81d4
Felipe <felipemachado572@gmail.com>
2025-02-03 19:59:31 -0300

    starting get method

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (request, response) => {
+    response.send("Hello, world!!")
+});
+
 const PORT = 3232;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
