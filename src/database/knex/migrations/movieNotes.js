313b0e6df36b8422cf74e29ffe8031970b398d24
Felipe <felipemachado572@gmail.com>
2025-02-25 20:08:28 -0300

    Create table movieNotes

--- src/database/knex/migrations/movieNotes.js
+++ src/database/knex/migrations/movieNotes.js
@@ -2,9 +2,9 @@ exports.up = knex => knex.schema.createTable("movieNotes", table => {
     table.increments("id");
     table.text("title");
     table.text("description");
-    table.interger("rating");
-    table.interger("user_id").reference("id").inTable("users");
-    
+    table.integer("rating");
+    table.integer("user_id").references("id").inTable("users");
+
     table.timestamp("created_at").default(knex.fn.now());
     table.timestamp("update_at").default(knex.fn.now());
 });
