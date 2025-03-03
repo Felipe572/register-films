43cff20c0fefdac03731adcdd94220431bb0f3ec
Felipe <felipemachado572@gmail.com>
2025-03-03 19:30:01 -0300

    Update de MovieNotes

--- src/routes/movieNotes.routes.js
+++ src/routes/movieNotes.routes.js
@@ -8,8 +8,8 @@ const movieNotesRoutes = Router();
 const movieNotesController = new MovieNotesController(); 
 
 movieNotesRoutes.post("/:user_id", movieNotesController.create);
+movieNotesRoutes.get("/", movieNotesController.index);
 movieNotesRoutes.get("/:id", movieNotesController.show);
 movieNotesRoutes.delete("/:id", movieNotesController.delete);
-
 
 module.exports = movieNotesRoutes;
